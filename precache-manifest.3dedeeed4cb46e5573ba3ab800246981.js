51990fffd4ac0614c6b549cc881a4a1d67205c63
Micky Woo <mwoo@seattleu.edu>
2020-09-01 15:39:48 -0700

    Updates

---
+++ precache-manifest.3dedeeed4cb46e5573ba3ab800246981.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7361e8798c43ed77dec8043e42ab621d",
+    "url": "/React-Project-1/index.html"
+  },
+  {
+    "revision": "a3d915020bc009a00e3a",
+    "url": "/React-Project-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/React-Project-1/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Project-1/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d915020bc009a00e3a",
+    "url": "/React-Project-1/static/js/main.ed75ced0.chunk.js"
+  },
+  {
+    "revision": "8617b4cc8e6b9f1aac59",
+    "url": "/React-Project-1/static/js/runtime-main.ac96bc94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Project-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
